8945d0ff9ef7bb63ceeeae2433affd32e22a8f30
MARCELO GUERRERO <christianmarcelog@gmail.com>
2022-06-02 19:06:15 -0500

    add project 4

--- PROJECT4/variables.tf
+++ PROJECT4/variables.tf
@@ -1,7 +1,9 @@
 variable "organization_id" {
   description = "The organization id for the associated services"
+  default    = "default"
 }
 
 variable "billing_account" {
   description = "The ID of the billing account to associate this project with"
+  default     = "default"
 }
