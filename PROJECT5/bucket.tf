e06415d4381823bf9a21b3c575b75402eeba8df9
MARCELO GUERRERO <christianmarcelog@gmail.com>
2022-06-13 14:53:49 -0500

    Project 5

--- PROJECT5/bucket.tf
+++ PROJECT5/bucket.tf
@@ -1,19 +1,12 @@
-provider "google" {
-project = var.project_id
-region  = var.region
-}
-
 # Create a GCS Bucket
-resource "google_storage_bucket" "my_bucket" {
-name     = var.bucket_name
-location = var.region
+resource "google_storage_bucket" "bucket" {
+  name       = var.bucket_name
+  location   = var.region
 }
-
 resource "google_storage_bucket" "my_bucket_set" {
   project       = var.project_id
-
   for_each      = toset(var.bucket_name_set)
-  name          = each.value  # note: each.key and each.value are the same for a set
+  name          = each.value #note:each.key and each.value are the same for a set
   location      = var.region
   storage_class = "STANDARD"
   force_destroy = true
